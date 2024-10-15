524dfee0f9ee67e54a6ce7eca526b3df29e33f19
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-10-15 20:59:57 +0800

    Create Solution.cpp

---
+++ Add Binary/Solution.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    std::string addBinary(std::string a, std::string b) {
+        std::string result = "";
+        int carry = 0;
+        
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+            
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+            
+            result += (sum % 2) + '0';
+            
+            carry = sum / 2;
+        }
+        
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
