ca6f427594317ccce787a9465ec2568c8d452fd9
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-10-27 23:33:47 +0800

    Create Solution.cpp

---
+++ Symmetric Tree/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        return isMirror(root->left, root->right);
+    }
+    
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        if (left->val != right->val) return false;
+        
+        return isMirror(left->left, right->right) && isMirror(left->right, right->left);
+    }
+};
