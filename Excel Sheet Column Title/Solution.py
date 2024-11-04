dc9fd5aba893d825a52b459205da0b294d2f64df
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-04 18:51:35 +0800

    Update Solution.py

--- Excel Sheet Column Title/Solution.py
+++ Excel Sheet Column Title/Solution.py
@@ -7,7 +7,7 @@ class Solution:
         while columnNumber > 0:
             columnNumber -= 1
             remainder = columnNumber % 26
-            result = chr(remainder + ord('A')) + result  # Convert remainder to corresponding character and prepend it
-            columnNumber //= 26  # Move to the next position
+            result = chr(remainder + ord('A')) + result
+            columnNumber //= 26
             
         return result
