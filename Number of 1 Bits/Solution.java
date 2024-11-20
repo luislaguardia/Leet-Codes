e3afd5a9c448d9a4d188ef30d393b7555a197ca9
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-20 22:45:11 +0800

    Update Solution.java

--- Number of 1 Bits/Solution.java
+++ Number of 1 Bits/Solution.java
@@ -1,11 +1,10 @@
 class Solution {
-public:
-    int hammingWeight(int n) {
+    public int hammingWeight(int n) {
         int count = 0;
-        while (n) {
-            count += n & 1; 
-            n >>= 1;
+        while (n != 0) {
+            n &= (n - 1);
+            count++;
         }
         return count;
     }
-};
+}
