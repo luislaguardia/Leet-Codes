4c3011973f010dfe86c3ea7ea15a21d8122e5986
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-20 22:45:17 +0800

    Update Solution.java

--- Number of 1 Bits/Solution.java
+++ Number of 1 Bits/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         while (n != 0) {
-            n &= (n - 1);
+            n &= (n - 1); // Clear the least significant set bit
             count++;
         }
         return count;
