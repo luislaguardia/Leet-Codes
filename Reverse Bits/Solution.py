b6ccafa86eed33e132104ad6e7248eb346d6086b
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-20 22:49:54 +0800

    Update Solution.py

--- Reverse Bits/Solution.py
+++ Reverse Bits/Solution.py
@@ -1,1 +1,18 @@
-a
+class Solution:
+    def __init__(self):
+        self.lookup = [0] * 256
+        for i in range(256):
+            self.lookup[i] = self.reverse8Bits(i)
+
+    def reverse8Bits(self, n):
+        result = 0
+        for i in range(8):
+            result = (result << 1) | (n & 1)
+            n >>= 1
+        return result
+    
+    def reverseBits(self, n: int) -> int:
+        return (self.lookup[n & 0xFF] << 24) | \
+               (self.lookup[(n >> 8) & 0xFF] << 16) | \
+               (self.lookup[(n >> 16) & 0xFF] << 8) | \
+               self.lookup[(n >> 24) & 0xFF]
