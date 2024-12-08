7e5d12fbe731fb23e6f49611176d0bd752c3d3e5
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-12-08 22:57:40 +0800

    Update Solution.cpp

--- Intersection of Two Linked Lists/Solution.cpp
+++ Intersection of Two Linked Lists/Solution.cpp
@@ -6,5 +6,19 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) return nullptr;
 
-class Solution 
+        ListNode *ptrA = headA;
+        ListNode *ptrB = headB;
+
+        while (ptrA != ptrB) {
+            ptrA = (ptrA != nullptr) ? ptrA->next : headB;
+            ptrB = (ptrB != nullptr) ? ptrB->next : headA;
+        }
+
+        return ptrA;
+    }
+};
