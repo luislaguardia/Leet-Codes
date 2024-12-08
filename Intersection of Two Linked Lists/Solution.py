fd284552cee10bc781dd2e63c9dbbca203125352
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-12-08 22:55:50 +0800

    Update Solution.py

--- Intersection of Two Linked Lists/Solution.py
+++ Intersection of Two Linked Lists/Solution.py
@@ -1,1 +1,12 @@
 class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+
+        ptrA, ptrB = headA, headB
+
+        while ptrA != ptrB:
+            ptrA = ptrA.next if ptrA else headB
+            ptrB = ptrB.next if ptrB else headA
+
+        return ptrA
