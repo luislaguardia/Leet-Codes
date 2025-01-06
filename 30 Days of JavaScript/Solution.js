939c08ce85c0ed1a5aa6db5aa843f1157563f38b
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2025-01-06 20:53:03 +0800

    Create Solution.js

---
+++ 30 Days of JavaScript/Solution.js
@@ -0,0 +1,15 @@
+/**
+ * TAG: #2667
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return ("Hello World");
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
