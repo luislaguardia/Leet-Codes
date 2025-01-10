34ddbeb482762c6e2c97bf338f15b513efbc26b4
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2025-01-10 15:33:05 +0800

    Create Solution.js

---
+++ 30 Days of JavaScript/Counter/Solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let current = n;
+
+    return function() {
+    const value = current;
+        current++;
+        return value;   
+    };
+};
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
